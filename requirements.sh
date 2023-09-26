414fb054fc9475a552868b630f31aef889eb61f7
Alexander-Hung <hungalex@hawaii.edu>
2023-09-26 02:44:52 -1000

    ip

--- requirements.sh
+++ requirements.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Check if Node.js and npm are installed
-if ! command -v node &> /dev/null
-then
-    echo "Node.js is not installed. Please install it first."
-    exit
-fi
-
-if ! command -v npm &> /dev/null
-then
-    echo "npm is not installed. Please install it first."
-    exit
-fi
-
 # Install Angular CLI
 npm install -g @angular/cli
 
