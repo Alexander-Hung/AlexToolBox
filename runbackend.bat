49219d4c399046cf115e51a06a49c00037f2160c
Alexander Hung <hungalex@hawaii.edu>
2023-09-25 15:41:23 -1000

    update

---
+++ runbackend.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM Navigate to the backend directory and start the server
+cd toolbox-backend
+start npm start
+
+exit
--- runfrontend.bat
+++ runfrontend.bat
@@ -1,11 +1,5 @@
 @echo off
 
-REM Navigate to the backend directory and start the server
-cd toolbox-backend
-start npm start
-
 REM Navigate to the frontend directory and serve the Angular app
 cd ..\toolbox-frontend
 ng serve
-
-exit
