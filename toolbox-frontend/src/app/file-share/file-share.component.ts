d48925e064b93b1ab158e8f58608d23b657b4030
Alexander Hung <hungalex@hawaii.edu>
2023-10-05 13:46:06 -1000

    Update file-share.component.ts

--- toolbox-frontend/src/app/file-share/file-share.component.ts
+++ toolbox-frontend/src/app/file-share/file-share.component.ts
@@ -16,7 +16,7 @@ export class FileShareComponent implements OnInit {
   }
 
   refreshFileList() {
-    this.http.get<string[]>('http://0.0.0.0:3000/files').subscribe(files => {
+    this.http.get<string[]>('http://localhost:3000/files').subscribe(files => {
       this.files = files;
       this.cd.detectChanges();
 
