ac4e376bab667ef4c9a2e1242c9cbc73d6cb96f7
Alexander Hung <hungalex@hawaii.edu>
2023-10-05 15:29:19 -1000

    Update app-routing.module.ts

--- toolbox-frontend/src/app/app-routing.module.ts
+++ toolbox-frontend/src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { HomeComponent } from './home/home.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { ServiceComponent } from "./service/service.component";
 import { FileListComponent } from './file-list/file-list.component';
-import { FileUploadComponent } from './file-upload/file-upload.component';
-import {FileShareComponent} from "./file-share/file-share.component";
 
 
 const routes: Routes = [
@@ -14,8 +12,6 @@ const routes: Routes = [
   { path: 'service', component: ServiceComponent },
   { path: 'calculator', component: CalculatorComponent },
   { path: 'share', component: FileShareComponent },
-  { path: 'files', component: FileListComponent },
-  { path: 'upload', component: FileUploadComponent }
 ];
 
 @NgModule({
