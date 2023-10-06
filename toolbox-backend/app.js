5a794ae354db4b8a5f4521828d4c9e6a8a8e4758
Alexander Hung <hungalex@hawaii.edu>
2023-10-05 15:27:22 -1000

    Update app.js

--- toolbox-backend/app.js
+++ toolbox-backend/app.js
@@ -73,7 +73,7 @@ app.delete('/delete/:filename', (req, res) => {
   });
 });
 
-app.listen(3000, '76.93.217.172',onServerListening);
+app.listen(3000, '192.168.50.13',onServerListening);
 
 app.use(cors({
   origin: 'http://http://76.93.217.172:4200',
