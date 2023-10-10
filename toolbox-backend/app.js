64d99511a2ad0d959212dabc76d5c18717bfa19f
Alexander Hung <hungalex@hawaii.edu>
2023-10-09 18:57:17 -1000

    test

--- toolbox-backend/app.js
+++ toolbox-backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
@@ -16,6 +17,61 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
+
+
+
+
+const storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  console.log("File upload endpoint hit!");
+  res.send('File uploaded!');
+});
+
+app.get('/download/:filename', (req, res) => {
+  const file = path.join(__dirname, '../uploads', req.params.filename);
+  if (!fs.existsSync(file)) {
+    console.error('File not found:', req.params.filename);
+    return res.status(404).send('File not found');
+  }
+  res.download(file);
+});
+
+app.get('/files', (req, res) => {
+  const directoryPath = path.join(__dirname, '../uploads');
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error('Error fetching file list:', err);
+      return res.status(500).send('Unable to scan directory: ' + err);
+    }
+    res.json(files);
+  });
+});
+
+app.delete('/delete/:filename', (req, res) => {
+  const file = path.join(__dirname, '../uploads', req.params.filename);
+  if (!fs.existsSync(file)) {
+    console.error('File not found:', req.params.filename);
+    return res.status(404).send('File not found');
+  }
+
+  fs.unlink(file, (err) => {
+    if (err) {
+      console.error('Error deleting file:', err);
+      return res.status(500).send('Error deleting file');
+    }
+    res.send('File deleted successfully');
+  });
+});
+
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
