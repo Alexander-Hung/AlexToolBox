108d854b14aade458508edc88510b69c8e1133f8
Alexander Hung <hungalex@hawaii.edu>
2023-10-09 15:44:12 -1000

    environment setting

--- toolbox-frontend/src/app/tools.service.ts
+++ toolbox-frontend/src/app/tools.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from './environment/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToolsService {
-  private baseUrl = 'http://76.93.217.172:3000';
 
   constructor(private http: HttpClient) { }
 
   getTools() {
-    return this.http.get<any>(`${this.baseUrl}/tools`);
+    return this.http.get<any>(`${environment.apiBaseUrl}/tools`);
   }
 }
