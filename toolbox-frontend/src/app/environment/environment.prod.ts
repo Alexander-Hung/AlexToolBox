6a92cfc7b25bfc9689e425eacef47f13e34db09e
Alexander Hung <hungalex@hawaii.edu>
2023-10-12 16:39:25 -1000

    api

--- toolbox-frontend/src/app/environment/environment.prod.ts
+++ toolbox-frontend/src/app/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://www.alextoolbox.work:5000', //http://76.93.217.172:5000
+  apiBaseUrl: 'https://76.93.217.172:5000', //http://76.93.217.172:5000
   password: 'test'
 };
