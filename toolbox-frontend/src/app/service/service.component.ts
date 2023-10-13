e22748b076531f6a9c212cda566f8cea9fbce6a9
Alexander Hung <hungalex@hawaii.edu>
2023-10-12 14:10:35 -1000

    Update service.component.ts

--- toolbox-frontend/src/app/service/service.component.ts
+++ toolbox-frontend/src/app/service/service.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ToolsService } from '../tools.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-Service',
@@ -10,17 +8,8 @@ import { CommonModule } from '@angular/common';
 export class ServiceComponent implements OnInit {
   tools: string[] = [];
 
-  constructor(private toolsService: ToolsService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.toolsService.getTools().subscribe(
-      data => {
-        console.log("Data received:", data);
-        this.tools = data.tools;
-      },
-      error => {
-        console.error("Error fetching tools:", error);
-      }
-    );
   }
 }
