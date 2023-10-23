755ba6dd09053c70d1bd9782e61d85a9ace72d32
Alexander Hung <hungalex@hawaii.edu>
2023-10-22 23:57:22 -1000

    Update why-fires.js

--- toolbox-backend/public/why-fires.js
+++ toolbox-backend/public/why-fires.js
@@ -78,6 +78,3 @@ app.get('/why-fires', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
-});
